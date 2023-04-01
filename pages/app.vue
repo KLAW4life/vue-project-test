9a0415941000932574b5bef0dd3d8c7bb5276b1d
KLAW4life <kerene789@gmail.com>
2023-04-01 09:53:43 -0500

    Added composition api

--- pages/app.vue
+++ pages/app.vue
@@ -13,7 +13,9 @@
             <v-img :src="card.image" aspect-ratio="1"></v-img>
             <v-card-title>{{ card.title }}</v-card-title>
             <v-card-text>{{ card.description }}</v-card-text>
-            <v-btn>
+            <v-btn
+              @click="test"
+            >
               Click Me to Like!
             </v-btn>
           </v-card>
@@ -25,39 +27,39 @@
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        cards: [
-          {
-            id: 1,
-            title: 'Card 1',
-            description: 'Description for card 1',
-            image: 'https://th.bing.com/th/id/OIG.Ex8vuUEwB_PsjepuIxRM?pid=ImgGn',
-          },
-          {
-            id: 2,
-            title: 'Card 2',
-            description: 'Description for card 2',
-            image: 'https://th.bing.com/th/id/OIG.LXzak5Qy5QSsJTwefUyo?w=270&h=270&c=6&r=0&o=5&dpr=1.3&pid=ImgGn',
-          },
-          {
-            id: 3,
-            title: 'Card 3',
-            description: 'Description for card 3',
-            image: 'https://th.bing.com/th/id/OIG.zuWbfzEoTNyhm67yxJl9?pid=ImgGn',
-          },
-          {
-            id: 4,
-            title: 'Card 4',
-            description: 'Description for card 4',
-            image: 'https://th.bing.com/th/id/OIG.Ek5JD2_1Jliu_X4wBiP.?pid=ImgGn',
-          },
-        ],
-      };
+<!-- Format to use composition api using setup in script tag -->
+<script setup> 
+  const cards = ref([
+    {
+      id: 1,
+      title: 'Card 1',
+      description: 'Description for card 1',
+      image: 'https://th.bing.com/th/id/OIG.Ex8vuUEwB_PsjepuIxRM?pid=ImgGn',
     },
-  };
+    {
+      id: 2,
+      title: 'Card 2',
+      description: 'Description for card 2',
+      image: 'https://th.bing.com/th/id/OIG.LXzak5Qy5QSsJTwefUyo?w=270&h=270&c=6&r=0&o=5&dpr=1.3&pid=ImgGn',
+    },
+    {
+      id: 3,
+      title: 'Card 3',
+      description: 'Description for card 3',
+      image: 'https://th.bing.com/th/id/OIG.zuWbfzEoTNyhm67yxJl9?pid=ImgGn',
+    },
+    {
+      id: 4,
+      title: 'Card 4',
+      description: 'Description for card 4',
+      image: 'https://th.bing.com/th/id/OIG.Ek5JD2_1Jliu_X4wBiP.?pid=ImgGn',
+    },
+  ])
+
+ function test() {
+  alert('Hello World')
+ }
+
 </script>
   
 <style>
