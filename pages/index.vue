4e0779a323652342afa4173786533d7ae9e9a3b4
KLAW4life <kerene789@gmail.com>
2023-04-02 18:30:49 -0500

    Fixed homepage navigation

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <p>Click a link below to navigate to another page.</p>
       <ul>
         <li><nuxt-link to="/app">AI Image Gallery</nuxt-link></li>
-        <li><nuxt-link to="/card-gallery">AI Image Full Library</nuxt-link></li>
+        <li><nuxt-link to="/card/gallery">AI Image Full Library</nuxt-link></li>
       </ul>
     </div>
 </template>
